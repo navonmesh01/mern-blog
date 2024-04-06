56810e7d0d37952c4d53a86c092a614083c00bac
navonmesh01 <140613455+navonmesh01@users.noreply.github.com>
2024-04-06 07:01:57 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,14 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
+
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
@@ -19,7 +22,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
       </Routes>
-    
+
     </BrowserRouter>
   )
 }
