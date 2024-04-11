c47bf5630acf69ccd497c0685645b04f9fb6061f
navonmesh01 <140613455+navonmesh01@users.noreply.github.com>
2024-04-11 09:22:04 +0530

    add google Oauth functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link,useNavigate } from 'react-router-dom';
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import Oauth from '../components/Oauth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -73,6 +74,7 @@ export default function SignUp() {
                 ) : ('SIGN UP'
               )}
             </Button>
+            <Oauth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an Account</span>
