338506d4d95ded099a5bd322a8b518cbfc721dad
navonmesh01 <140613455+navonmesh01@users.noreply.github.com>
2024-07-19 04:15:01 +0530

    show users to admin dashboard

--- client/src/components/Dashsidebar.jsx
+++ client/src/components/Dashsidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Sidebar} from 'flowbite-react';
-import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import { useState,useEffect } from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -48,6 +48,13 @@ export default function Dashsidebar() {
                   <Link to='/dashboard?tab=posts'>
                   <Sidebar.Item active={tab==='posts'} icon={HiDocumentText} as='div' >
                     Posts
+                  </Sidebar.Item>
+                </Link>
+                )}
+                {currentUser.isAdmin && (
+                  <Link to='/dashboard?tab=users'>
+                  <Sidebar.Item active={tab==='users'} icon={HiOutlineUserGroup} as='div' >
+                    Users
                   </Sidebar.Item>
                 </Link>
                 )}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import {useLocation} from 'react-router-dom';
 import DashProfile from '../components/DashProfile';
 import Dashsidebar from '../components/Dashsidebar';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
+
 
 export default function Dashboard() {
   const location = useLocation();
@@ -28,6 +30,9 @@ export default function Dashboard() {
 
       {/* posts */}
       {tab === 'posts' && <DashPosts/>}
+
+      {/* users */}
+      {tab === 'users' && <DashUsers/>}
     </div>
   )
 }
