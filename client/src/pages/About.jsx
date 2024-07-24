933f134f6e5c3514adc73b9daff7cd9bc8070316
navonmesh01 <140613455+navonmesh01@users.noreply.github.com>
2024-07-24 08:18:50 +0530

    Complete About and Project Pages(to be edited)

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div >
+          <h1 className="text-3xl font-semibold text-center my-7">About Navam's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+              officiis corporis quia alias velit vero laboriosam, pariatur,
+              dicta vel nam quam porro perferendis dolorem assumenda modi
+              eveniet nisi voluptates ipsa. Rerum nulla omnis sequi deleniti,
+              repellat eaque ducimus quidem cumque, molestiae provident sunt
+              placeat doloribus repellendus aspernatur adipisci magni.
+              Dignissimos!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+              officiis corporis quia alias velit vero laboriosam, pariatur,
+              dicta vel nam quam porro perferendis dolorem assumenda modi
+              eveniet nisi voluptates ipsa. Rerum nulla omnis sequi deleniti,
+              repellat eaque ducimus quidem cumque, molestiae provident sunt
+              placeat doloribus repellendus aspernatur adipisci magni.
+              Dignissimos!
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate excepturi est earum, reprehenderit laudantium, non
+              aperiam autem dolore nesciunt itaque pariatur suscipit quos
+              facilis aspernatur dignissimos doloremque repudiandae voluptas
+              beatae iusto ad dolorum velit sint adipisci. Quas, officiis sunt
+              ipsa molestias dolore ad tempora eum quis doloremque quam? Maxime,
+              excepturi?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import CallToAction from "../components/CallToAction";
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex flex-col justify-center items-center gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>Lorem ipsum dolor, sit amet consect sdewf rege rgiuk tjtyjerg gr rgerg greetur adipisicing elit. Veritatis iure labore assas veniam, voluptate quia alias!</p>
+      <CallToAction />
+    </div>
   )
 }
